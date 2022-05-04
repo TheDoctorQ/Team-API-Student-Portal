f584f1c74356db521693e14a545ffd86ed860e2c
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-05-04 10:28:04 -0600

    adds page links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/resume">Resume</router-link> |
+    <router-link to="/capstone">Capstone</router-link>
+
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
