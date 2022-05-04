26a251c4a7fe6bca6607d13d98e1665a9db63863
Kaz <takaik@yahoo.com>
2022-05-04 12:55:39 -0400

    get data (experiences, educations, skills and student)

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue'
+import ResumeView from '@/views/ResumeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: ResumeView
   },
   {
     path: '/about',
