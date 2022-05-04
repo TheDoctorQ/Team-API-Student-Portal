1bff7e8256bbb4e617e989184d42ec57541012a4
Kaz <takaik@yahoo.com>
2022-05-04 12:19:59 -0400

    init view files

---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style>
+  
+</style>
